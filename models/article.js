03ef46a251a4a1c33a1f964f0c0ca8408bdade89
ehager77 <eric@isabel.io>
2019-04-29 21:57:04 -0700

    added models and server

---
+++ models/article.js
@@ -0,0 +1,37 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema ({
+	article: {
+		type: String,
+		unique: true
+	},
+	summary: {
+		type: String
+	},
+	link: {
+		type: String
+	},
+	photo: {
+		type: String
+	},
+	date: {
+		type: Date
+	},
+	saved: {
+		type: Boolean,
+		default: false
+	},
+	note: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Note"
+		}
+	]
+});
+
+const article = mongoose.model("article", articleSchema);
+
+module.exports = article;
